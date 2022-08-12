5a1b8c305749bb2ccbff85f88d42ef74fdd3871f
SUNITA DAS MITRA <110525289+SUNITADAS078@users.noreply.github.com>
2022-08-12 18:26:08 +0530

    Add files via upload

---
+++ q4.java
@@ -0,0 +1,10 @@
+//write a java program to computer the specified expressions and print the output.
+
+public class Expression
+   {
+ 
+    public static void main(String[] arg) {
+
+        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+
+    }}
---
+++ swap.java
@@ -0,0 +1,16 @@
+//swapping two variables
+
+ class Swapping {
+ 
+ public static void main(String[] args) {
+     
+   int a, b, temp;
+   a = 20;
+   b = 25;
+   System.out.println("Before swapping : a, b = "+a+", "+ + b);
+   temp = a;
+   a = b;
+   b = temp;   
+  System.out.println("After swapping : a, b = "+a+", "+ + b);
+ }}
+
