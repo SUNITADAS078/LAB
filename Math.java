31a27d3bfa090dd7eb5bf4a0ea2423bb350e9966
SUNITA DAS MITRA <110525289+SUNITADAS078@users.noreply.github.com>
2022-08-12 19:50:51 +0530

    Add files via upload

---
+++ Math.java
@@ -0,0 +1,23 @@
+//Write a Java program to print the sum (addition),subtract,multiply,divide and remainder of two numbers.
+//Input 1st number:50
+//Input 2nd number:10
+
+class Math{
+public static void main(String[] args){
+System.out.println("Enter two numbers :");
+int a=50;
+int b=10;
+System.out.println(" your two no is =" +a +" " +b);
+int sum=a+b;
+int sub=a-b;
+int mul=a*b;
+int div=a/b;
+int rem=a%b;
+System.out.println("your sum is =" +sum);
+System.out.println("your subtraction is =" +sub);
+System.out.println("your multiplication is =" +mul);
+System.out.println("your divide is =" +div);
+System.out.println("your remainder is =" +rem);
+   }
+}
+
